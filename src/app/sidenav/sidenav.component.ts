72fc8d85ff63a0d62c708b33e47b5d6f174c00b3
Irakli Adamashvili <adamashvili.irakli8@gmail.com>
2023-04-12 19:44:43 +0400

    Sidenav finish

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { navbarData } from './nav-data';
 
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent {
-collapsed = true;
+  @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter
+collapsed = false;
+screenWidth = 0;
 navData = navbarData;
 
 toggleCollapse(): void {
   this.collapsed = !this.collapsed;
+  this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
 }
 
 closeSidenav(): void {
   this.collapsed = false;
+  this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
 }
 
 }
