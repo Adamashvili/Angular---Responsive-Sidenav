8520b616e10543bc5bcc195e03b583ae24acae2d
Irakli Adamashvili <adamashvili.irakli8@gmail.com>
2023-04-12 20:38:50 +0400

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { CoupensComponent } from './coupens/coupens.component';
+import { PagesComponent } from './pages/pages.component';
+import { MediaComponent } from './media/media.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'statistics', component: StatisticsComponent},
+  {path: 'coupes', component: CoupensComponent},
+  {path: 'pages', component: PagesComponent},
+  {path: 'media', component: MediaComponent},
+  {path: 'settings', component: SettingsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-body',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./body.component.scss']
 })
 export class BodyComponent {
+@Input() collapsed = false;
+@Input() screenWidth = 0;
+
+  getBodyClass(): String {
+    let styleClass = '';
+    if(this.collapsed && this.screenWidth > 768) {
+      styleClass = 'body-trimmed'; }
+       else if (this.collapsed && this.screenWidth <= 768 && this.screenWidth > 0) {
+        styleClass =  'body-md-screen'
+    }
+    
+    return styleClass;
+
+  }
 
 }
